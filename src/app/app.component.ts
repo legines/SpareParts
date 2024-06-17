36bf4ce3f778fcdbce50d04d55367ee8cdc65978
Luis Gines <legines@gmail.com>
2024-06-16 23:19:08 -0400

    Header / initial post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'spare-parts';
+export class AppComponent implements OnInit {
+  logoArray: string[] = ['assets/SpareParts-black-gold.svg', 'assets/SpareParts-black-text-only.svg', 'assets/SpareParts-black.svg', 'assets/SpareParts-bright-white.svg', 'assets/SpareParts-gold.svg', 'assets/SpareParts-off-white.svg', 'assets/SpareParts-white-text-only.svg', 'assets/SpareParts-purple.svg'];
+  logoUrl: string = ''
+
+  constructor() {}
+
+  ngOnInit() {
+    this.logoUrl = 'assets/SpareParts-black-gold.svg';
+    setInterval(() => {
+      let random = Math.floor(Math.random() * this.logoArray.length);
+       this.logoUrl = this.logoArray[random];
+    }, 2000);
+  }
 }
