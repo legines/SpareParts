a55e37b924dbff3b7f2bf15b0896edd73d5dee53
Luis Gines <legines@gmail.com>
2025-12-08 22:20:49 -0500

    Updating post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_one_attached :cover_image
+  has_one_attached :cover_image, dependent: :destroy
   has_rich_text :content
   validates :title, presence: true
 end
